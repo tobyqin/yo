c8cf92b3fe80523475f2fe506046e34b26604ef4
Toby Qin <toby.qin@live.com>
2020-07-07 22:58:06 +0800

    add pyyaml as dependencies.

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click'
+        'Click',
+        'pyyaml'
     ],
     entry_points='''
         [console_scripts]
